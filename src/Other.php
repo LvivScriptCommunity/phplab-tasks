67b18616349bfb8a9a20e9f588e70a0758cc322a
Acrodim <61546699+Acrodim@users.noreply.github.com>
2021-02-03 01:13:45 +0200

    Delete Other.php

--- src/Other.php
+++
@@ -1,9 +0,0 @@
-<?php
-array_multisort($input);
-$newArr =[];
-foreach ($arr as $item){
-    $newArr = array_merge_recursive($newArr, array_combine ( $item['tags'], array_fill(0, count($item['tags']), $item['name'])));
-}
-ksort($newArr);
-
-var_dump( $newArr);
