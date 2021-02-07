7272da03a07daed1a82c61a06ab738c059be62a6
anas-alali-24 <anas-alali-24@users.noreply.github.com>
2021-02-07 21:41:30 +0200

    home work

--- src/web/functions.php
+++ src/web/functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The $airports variable contains array of arrays of airports (see airports.php)
  * What can be put instead of placeholder so that function returns the unique first letter of each airport name
@@ -6,12 +7,84 @@
  *
  * Create a PhpUnit test (GetUniqueFirstLettersTest) which will check this behavior
  *
- * @param  array  $airports
+ * @param  array $airports
  * @return string[]
+ * array $airports
  */
-function getUniqueFirstLetters(array $airports)
+function getUniqueFirstLetters(array $airports): array
 {
-    // put your logic here
+    // Put your logic here.
+    $retunarray = [];
+    foreach ($airports as $airport) {
+        array_push($retunarray, substr($airport['name'], 0, 1));
+    }
 
-    return ['A', 'B', 'C'];
+    $retunarray = array_unique($retunarray);
+    asort($retunarray);
+    return $retunarray;
 }
+//end getUniqueFirstLetters()
+
+function filterByBirstLetter($firstletter, array $airports): array
+{
+    $retunarray = [];
+    foreach ($airports as $airport) {
+        if (substr($airport['name'], 0, 1) == $firstletter) {
+            array_push($retunarray, $airport);
+        }
+    }
+
+    asort($retunarray);
+    return($retunarray);
+}
+//end filterByBirstLetter()
+
+// filter_by_state
+function filterByState($state, array $airports): array
+{
+    $retunarray = [];
+    foreach ($airports as $airport) {
+        if ($airport['state'] == $state) {
+            array_push($retunarray, $airport);
+        }
+    }
+
+    asort($retunarray);
+    return($retunarray);
+}
+//end filterByState()
+
+function updateUrl($url, $param = ''): string
+{
+    $url_parts = parse_url($url);
+    if (isset($url_parts['query'])) {
+        // Avoid 'Undefined index: query'
+        parse_str($url_parts['query'], $params);
+    } else {
+        $params = [];
+    }
+
+    if ($param != '' && $param != 'page') {
+        unset($params[$param]);
+        $params['page'] = '0';
+    } elseif ($param == 'page') {
+        unset($params[$param]);
+    }
+
+    $url_parts['query'] = http_build_query($params);
+    $url                = $url_parts['path'] . '?' . $url_parts['query'];
+    return $url;
+}
+//end updateUrl()
+
+function sort_By($sort, array $airports): array
+{
+    usort(
+        $airports,
+        function ($item1, $item2) use ($sort) {
+            return ($item2[$sort] <=> $item1[$sort]);
+        }
+    );
+    return $airports;
+}
+//end sort_By()
