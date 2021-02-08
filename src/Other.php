808711408f52270a6d3a99230d8254435d643ec7
Acrodim <acrodim@gmail.com>
2021-02-08 18:40:01 +0200

    merge with GitHub before WEB push

--- src/Other.php
+++ src/Other.php
@@ -1,9 +1,212 @@
 <?php
-array_multisort($input);
-$newArr =[];
-foreach ($arr as $item){
-    $newArr = array_merge_recursive($newArr, array_combine ( $item['tags'], array_fill(0, count($item['tags']), $item['name'])));
-}
-ksort($newArr);
+require_once './functions.php';
 
-var_dump( $newArr);
+$airports = require './airports.php';
+
+// Filtering
+/**
+ * Here you need to check $_GET request if it has any filtering
+ * and apply filtering by First Airport Name Letter and/or Airport State
+ * (see Filtering tasks 1 and 2 below)
+ */
+// Записываем в переменную GET-параметры для формирования массивов с фильтрацией и сортировкой
+$get = $_GET;
+
+// Формируем массив $airports в зависимости от GET-параметра filter_by_first_letter
+if (isset($_GET['filter_by_first_letter'])) {
+
+    $airports_by_letter = [];
+
+    // Записываем в массив те элементы массива, у которых первая буква значения ключа 'name' равна значению
+    //get-параметра 'filter_by_first_letter'
+    foreach ($airports as $key => $airport) {
+        if ($airport['name'][0] == $_GET['filter_by_first_letter']) {
+            $airports_by_letter[] = $airport;
+        }
+    }
+
+    $airports = $airports_by_letter;
+}
+
+// Формируем массив $airports в зависимости от GET-параметра filter_by_state
+if (isset($_GET['filter_by_state'])) {
+
+    $airports_by_state = [];
+
+    // Записываем в массив те элементы массива, у которых первая буква значения ключа 'state' равна значению
+    //get-параметра 'filter_by_state'
+    foreach ($airports as $key => $airport) {
+        if ($airport['state'][0] == $_GET['filter_by_state']) {
+            $airports_by_state[] = $airport;
+        }
+    }
+
+    $airports = $airports_by_state;
+}
+
+// Sorting
+/**
+ * Here you need to check $_GET request if it has sorting key
+ * and apply sorting
+ * (see Sorting task below)
+ */
+// Сортируем массив $airports в зависимости от GET-параметра sort
+if (isset($_GET['sort'])) {
+
+    $airports_sort = [];
+
+    foreach ($airports as $key => $airport) {
+        $airports_sort[$key] = $airport[$_GET['sort']];
+    }
+
+    if (!empty($airports_sort)) {
+        array_multisort($airports_sort, SORT_ASC, $airports);
+    }
+}
+
+// Pagination
+/**
+ * Here you need to check $_GET request if it has pagination key
+ * and apply pagination logic
+ * (see Pagination task below)
+ */
+// Определяем текущую страницу
+if (isset($_GET['page'])) {
+    $page = intval($_GET['page']);
+} else {
+    $page = 1;
+}
+
+// Формирование списка аэропортов для пагинации
+$airportsPerPage = 5; // Количество аэропортов на одной странице
+$pageQty = ceil(count($airports) / $airportsPerPage); // Общее количество страниц
+
+// Распределяем массив airports на общее количество страниц
+if ($page >= 1 && $page <= $pageQty) {
+    $from = ($page - 1) * $airportsPerPage;
+    $airports = array_slice($airports, $from, $airportsPerPage);
+
+} else {
+    $airports = [];
+}
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <title>Airports</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+</head>
+<body>
+<main role="main" class="container">
+
+    <h1 class="mt-5">US Airports</h1>
+
+    <!--
+        Filtering task #1
+        Replace # in HREF attribute so that link follows to the same page with the filter_by_first_letter key
+        i.e. /?filter_by_first_letter=A or /?filter_by_first_letter=B
+
+        Make sure, that the logic below also works:
+         - when you apply filter_by_first_letter the page should be equal 1
+         - when you apply filter_by_first_letter, than filter_by_state (see Filtering task #2) is not reset
+           i.e. if you have filter_by_state set you can additionally use filter_by_first_letter
+    -->
+
+    <div class="alert alert-dark">
+        Filter by first letter:
+
+        <?php foreach (getUniqueFirstLetters(require './airports.php') as $letter): ?>
+
+            <a href="?page=1<?php echo getLink($get, ['filter_by_first_letter' => $letter]) ?>"><?= $letter ?></a>
+
+        <?php endforeach; ?>
+
+        <a href="/?page=1" class="float-right">Reset all filters</a>
+    </div>
+
+    <!--
+        Sorting task
+        Replace # in HREF so that link follows to the same page with the sort key with the proper sorting value
+        i.e. /?sort=name or /?sort=code etc
+
+        Make sure, that the logic below also works:
+         - when you apply sorting pagination and filtering are not reset
+           i.e. if you already have /?page=2&filter_by_first_letter=A after applying sorting the url should looks like
+           /?page=2&filter_by_first_letter=A&sort=name
+    -->
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col"><a href="?page=<?php echo $_GET['page'] . getLink($get, ['sort' => 'name']) ?>">Name</a>
+            </th>
+            <th scope="col"><a href="?page=<?php echo $_GET['page'] . getLink($get, ['sort' => 'code']) ?>">Code</a>
+            </th>
+            <th scope="col"><a href="?page=<?php echo $_GET['page'] . getLink($get, ['sort' => 'state']) ?>">State</a>
+            </th>
+            <th scope="col"><a href="?page=<?php echo $_GET['page'] . getLink($get, ['sort' => 'city']) ?>">City</a>
+            </th>
+            <th scope="col">Address</th>
+            <th scope="col">Timezone</th>
+        </tr>
+        </thead>
+        <tbody>
+        <!--
+            Filtering task #2
+            Replace # in HREF so that link follows to the same page with the filter_by_state key
+            i.e. /?filter_by_state=A or /?filter_by_state=B
+
+            Make sure, that the logic below also works:
+             - when you apply filter_by_state the page should be equal 1
+             - when you apply filter_by_state, than filter_by_first_letter (see Filtering task #1) is not reset
+               i.e. if you have filter_by_first_letter set you can additionally use filter_by_state
+        -->
+        <?php foreach ($airports as $airport): ?>
+            <tr>
+                <td><?= $airport['name'] ?></td>
+                <td><?= $airport['code'] ?></td>
+                <td>
+                    <a href="?page=1<?php echo getLink($get, ['filter_by_state' => $airport['state'][0]]) ?>">
+                        <?= $airport['state'] ?></a>
+                </td>
+                <td><?= $airport['city'] ?></td>
+                <td><?= $airport['address'] ?></td>
+                <td><?= $airport['timezone'] ?></td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+
+    <!--
+        Pagination task
+        Replace HTML below so that it shows real pages dependently on number of airports after all filters applied
+
+        Make sure, that the logic below also works:
+         - show 5 airports per page
+         - use page key (i.e. /?page=1)
+         - when you apply pagination - all filters and sorting are not reset
+    -->
+
+    <nav aria-label="Navigation">
+        <ul class="pagination justify-content-center d-flex flex-wrap">
+
+            <?php for ($i = 1; $i <= $pageQty; $i++): ?>
+
+                <?php if ($i == $page): ?>
+                    <li class="page-item active"><a class="page-link"
+                                                    href="?page=<?= $i . getLink($get) ?>"><?= $i ?></a></li>
+                <?php else: ?>
+                    <li class="page-item"><a class="page-link" href="?page=<?= $i . getLink($get) ?>"><?= $i ?></a></li>
+                <?php endif ?>
+
+            <?php endfor ?>
+
+        </ul>
+    </nav>
+
+</main>
+</html>
