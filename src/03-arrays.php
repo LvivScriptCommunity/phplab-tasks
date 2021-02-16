40841ca71328550e9b50a587758d7157c4fa3c1f
Sergii Iunash <unassergej@gmail.com>
2021-02-16 17:18:51 +0200

    Add files via upload

---
+++ src/03-arrays.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * The $input variable contains an array of digits
+ * Return an array which will contain the same digits but repetitive by its value
+ * without changing the order.
+ * Example: [1,3,2] => [1,3,3,3,2,2]
+ *
+ * @param  array  $input
+ * @return array
+ */
+function repeatArrayValues(array $input)
+{
+    $arr = [];
+    foreach ($input as $value) {
+        for ($i = 0; $i < $value; $i++) {
+            $arr[] = $value;
+        }
+    }
+    return $arr;
+}
+
+/**
+ * The $input variable contains an array of digits
+ * Return the lowest unique value or 0 if there is no unique values or array is empty.
+ * Example: [1, 2, 3, 2, 1, 5, 6] => 3
+ *
+ * @param  array  $input
+ * @return int
+ */
+function getUniqueValue(array $input)
+{
+    $uniqueVal = array_unique($input);
+    $arrKey = array_diff_key($input, $uniqueVal);
+    $res = [];
+
+    foreach ($uniqueVal as $value) {
+        if (in_array($value, $arrKey)) {
+            $res[] = $value;
+        }
+    }
+
+    if (empty($res)) {
+        return 0;
+    }
+
+    return min($res);
+}
+
+/**
+ * The $input variable contains an array of arrays
+ * Each sub array has keys: name (contains strings), tags (contains array of strings)
+ * Return the list of names grouped by tags
+ * !!! The 'names' in returned array must be sorted ascending.
+ *
+ * Example:
+ * [
+ *  ['name' => 'potato', 'tags' => ['vegetable', 'yellow']],
+ *  ['name' => 'apple', 'tags' => ['fruit', 'green']],
+ *  ['name' => 'orange', 'tags' => ['fruit', 'yellow']],
+ * ]
+ *
+ * Should be transformed into:
+ * [
+ *  'fruit' => ['apple', 'orange'],
+ *  'green' => ['apple'],
+ *  'vegetable' => ['potato'],
+ *  'yellow' => ['orange', 'potato'],
+ * ]
+ *
+ * @param  array  $input
+ * @return array
+ */
+function groupByTag(array $input)
+{
+    $result = [];
+
+    foreach ($input as $i => $v) {
+        foreach ($v['tags'] as $tag) {
+            $result[$tag][] = $v['name'];
+        }
+    }
+    return $result;
+}
