a1b5663026e9d2a8a330b972c843de7ff6737630
mariandanyliv <danulivmarian40@gmail.com>
2021-02-21 23:32:50 +0200

    DBphp done

--- src/db/index.php
+++ src/db/index.php
@@ -2,14 +2,44 @@
 /**
  * Connect to DB
  */
-
+include 'pdo_ini.php';
+define( 'ITEMS_PER_PAGE', 10 );
 /**
  * SELECT the list of unique first letters using https://www.w3resource.com/mysql/string-functions/mysql-left-function.php
  * and https://www.w3resource.com/sql/select-statement/queries-with-distinct.php
  * and set the result to $uniqueFirstLetters variable
  */
-$uniqueFirstLetters = ['A', 'B', 'C'];
+$uniqueFirstLetters = getFirstLettersSql($pdo);;
 
+function getFirstLettersSql($pdo){
+    $sth = $pdo->prepare("SELECT DISTINCT LEFT(name, 1) AS letter FROM airports ORDER BY LEFT(name, 1)");
+    $sth->setFetchMode(\PDO::FETCH_ASSOC);
+    $sth->execute();
+    $items = $sth->fetchAll();
+    $letters = [];
+    foreach ($items as $item){
+        $letters[] = $item['letter'];
+    }
+
+    return $letters;
+}
+
+function addQueryArgs( $key, $value){
+    $get_args = $_GET;
+    $get_args[$key] = $value;
+    $i = 0;
+    $ulr_params = '';
+    foreach ($get_args as $k => $v){
+        if($i === 0){
+            $ulr_params .= "?{$k}={$v}";
+        }else{
+            $ulr_params .= "&{$k}={$v}";
+        }
+        $i++;
+    }
+
+    return $ulr_params;
+}
 // Filtering
 /**
  * Here you need to check $_GET request if it has any filtering
@@ -20,6 +50,21 @@ $uniqueFirstLetters = ['A', 'B', 'C'];
  * For filtering by state you will need to JOIN states table and check if states.name = A
  * where A - requested filter value
  */
+function getOrderParams(){
+    $order_by = '';
+    $filter = isset($_GET['filter']) ? $_GET['filter'] : '';
+    if($filter === 'name'){
+        $order_by = " ORDER BY a.name";
+    }elseif ($filter === 'code'){
+        $order_by = " ORDER BY a.code";
+    }elseif ($filter === 'state'){
+        $order_by = " ORDER BY s.name";
+    }elseif ($filter === 'city'){
+        $order_by = " ORDER BY c.name";
+    }
+    return $order_by;
+}
+
 
 // Sorting
 /**
@@ -30,6 +75,24 @@ $uniqueFirstLetters = ['A', 'B', 'C'];
  * For sorting use ORDER BY A
  * where A - requested filter value
  */
+function getWhereParams(){
+    $where = '';
+    $filter_by_first_letter = isset($_GET['filter_by_first_letter']) ? $_GET['filter_by_first_letter'] : false;
+    $filter_by_state = isset($_GET['filter_by_state']) ? $_GET['filter_by_state'] : false;
+    if($filter_by_first_letter || $filter_by_state){
+        $where = ' WHERE';
+    }
+    if($filter_by_first_letter) {
+        $where .= " a.name LIKE '{$filter_by_first_letter}%'";
+    }
+    if($filter_by_state) {
+        if($filter_by_first_letter) {
+            $where .= ' AND';
+        }
+        $where .= " s.name LIKE '{$filter_by_state}%'";
+    }
+    return $where;
+}
 
 // Pagination
 /**
@@ -40,6 +103,15 @@ $uniqueFirstLetters = ['A', 'B', 'C'];
  * For pagination use LIMIT
  * To get the number of all airports matched by filter use COUNT(*) in the SELECT statement with all filters applied
  */
+function getPagination($pdo){
+    $where = getWhereParams();
+    $order_by = '';
+    $sth = $pdo->prepare("SELECT COUNT(*) FROM airports AS a JOIN states AS s ON a.state_id = s.id  {$where} {$order_by}");
+    $sth->setFetchMode(\PDO::FETCH_ASSOC);
+    $sth->execute();
+    $items = $sth->fetchColumn();
+    return ceil($items/ITEMS_PER_PAGE);
+}
 
 /**
  * Build a SELECT query to DB with all filters / sorting / pagination
@@ -47,7 +119,35 @@ $uniqueFirstLetters = ['A', 'B', 'C'];
  *
  * For city_name and state_name fields you can use alias https://www.mysqltutorial.org/mysql-alias/
  */
-$airports = [];
+function getAirports($pdo){
+    $where = getWhereParams();
+    $order_by = getOrderParams();
+    $end = getLimitsAndOffset();
+    $paginate = getPagination($pdo);
+    if($end['page'] > $paginate){
+        $end['offset'] = ' OFFSET 0';
+    }
+    $sth = $pdo->prepare("SELECT a.*, s.name AS state_name, LEFT(s.name, 1) AS state_first, c.name AS city_name  
+                            FROM airports AS a JOIN states AS s ON a.state_id = s.id 
+                            JOIN cities AS c ON a.city_id = c.id {$where} {$order_by} {$end['limit']} {$end['offset']}");
+    $sth->setFetchMode(\PDO::FETCH_ASSOC);
+    $sth->execute();
+    $items = $sth->fetchAll();
+    return $items;
+}
+
+function getLimitsAndOffset(){
+    $page = isset($_GET['page']) ? $_GET['page'] : 1;
+    $ofset = $page - 1;
+    $limit = ITEMS_PER_PAGE;
+    return [
+        'limit' => " LIMIT {$limit}",
+        'offset' => " OFFSET {$ofset}",
+        'page' => floatval($page)
+    ];
+}
+
+$airports = getAirports($pdo);
 ?>
 <!doctype html>
 <html lang="en">
@@ -68,7 +168,6 @@ $airports = [];
         Filtering task #1
         Replace # in HREF attribute so that link follows to the same page with the filter_by_first_letter key
         i.e. /?filter_by_first_letter=A or /?filter_by_first_letter=B
-
         Make sure, that the logic below also works:
          - when you apply filter_by_first_letter the page should be equal 1
          - when you apply filter_by_first_letter, than filter_by_state (see Filtering task #2) is not reset
@@ -78,7 +177,7 @@ $airports = [];
         Filter by first letter:
 
         <?php foreach ($uniqueFirstLetters as $letter): ?>
-            <a href="#"><?= $letter ?></a>
+            <a href="<?= addQueryArgs('filter_by_first_letter', $letter) ?>"><?= $letter ?></a>
         <?php endforeach; ?>
 
         <a href="/" class="float-right">Reset all filters</a>
@@ -88,7 +187,6 @@ $airports = [];
         Sorting task
         Replace # in HREF so that link follows to the same page with the sort key with the proper sorting value
         i.e. /?sort=name or /?sort=code etc
-
         Make sure, that the logic below also works:
          - when you apply sorting pagination and filtering are not reset
            i.e. if you already have /?page=2&filter_by_first_letter=A after applying sorting the url should looks like
@@ -97,10 +195,10 @@ $airports = [];
     <table class="table">
         <thead>
         <tr>
-            <th scope="col"><a href="#">Name</a></th>
-            <th scope="col"><a href="#">Code</a></th>
-            <th scope="col"><a href="#">State</a></th>
-            <th scope="col"><a href="#">City</a></th>
+            <th scope="col"><a href="<?= addQueryArgs('filter', 'name') ?>">Name</a></th>
+            <th scope="col"><a href="<?= addQueryArgs('filter', 'code') ?>">Code</a></th>
+            <th scope="col"><a href="<?= addQueryArgs('filter', 'state') ?>">State</a></th>
+            <th scope="col"><a href="<?= addQueryArgs('filter', 'city') ?>">City</a></th>
             <th scope="col">Address</th>
             <th scope="col">Timezone</th>
         </tr>
@@ -110,7 +208,6 @@ $airports = [];
             Filtering task #2
             Replace # in HREF so that link follows to the same page with the filter_by_state key
             i.e. /?filter_by_state=A or /?filter_by_state=B
-
             Make sure, that the logic below also works:
              - when you apply filter_by_state the page should be equal 1
              - when you apply filter_by_state, than filter_by_first_letter (see Filtering task #1) is not reset
@@ -120,7 +217,7 @@ $airports = [];
         <tr>
             <td><?= $airport['name'] ?></td>
             <td><?= $airport['code'] ?></td>
-            <td><a href="#"><?= $airport['state_name'] ?></a></td>
+            <td><a href="<?= addQueryArgs('filter_by_state', $airport['state_first']) ?>"><?= $airport['state_name'] ?></a></td>
             <td><?= $airport['city_name'] ?></td>
             <td><?= $airport['address'] ?></td>
             <td><?= $airport['timezone'] ?></td>
@@ -132,19 +229,26 @@ $airports = [];
     <!--
         Pagination task
         Replace HTML below so that it shows real pages dependently on number of airports after all filters applied
-
         Make sure, that the logic below also works:
          - show 5 airports per page
          - use page key (i.e. /?page=1)
          - when you apply pagination - all filters and sorting are not reset
     -->
-    <nav aria-label="Navigation">
-        <ul class="pagination justify-content-center">
-            <li class="page-item active"><a class="page-link" href="#">1</a></li>
-            <li class="page-item"><a class="page-link" href="#">2</a></li>
-            <li class="page-item"><a class="page-link" href="#">3</a></li>
-        </ul>
-    </nav>
+    <?php $page = (isset($_GET['page']) && !empty($_GET['page'])) ? $_GET['page'] : 1; ?>
+    <?php $page_count = getPagination($pdo); ?>
+    <?php if($page_count > 1){ ?>
+        <nav aria-label="Navigation">
+            <ul class="pagination justify-content-center">
+                <?php for( $i=1; $i <= $page_count; $i++ ) { ?>
+                    <?php if( $page == $i) { ?>
+                        <li class="page-item active"><a class="page-link"><?= $i ?></a></li>
+                    <?php } else { ?>
+                        <li class="page-item"><a class="page-link" href="<?= addQueryArgs('page', $i ) ?>"><?= $i ?></a></li>
+                    <?php } ?>
+                <?php } ?>
+            </ul>
+        </nav>
+    <?php } ?>
 
 </main>
 </html>
